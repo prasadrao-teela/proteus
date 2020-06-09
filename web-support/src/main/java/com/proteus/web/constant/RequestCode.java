cccfde39796ea9effc645368344a6d8eb33f64fe
prasadaraoteela <prasadaraoteela@gmail.com>
2020-06-09 16:52:54 +0530

    Updated request code

--- web-support/src/main/java/com/proteus/web/constant/RequestCode.java
+++ web-support/src/main/java/com/proteus/web/constant/RequestCode.java
@@ -4,5 +4,5 @@ package com.proteus.web.constant;
  * Created by Prasad Rao on 09-06-2020 13:12
  **/
 public class RequestCode {
-    public static final int OPEN_FILE_CHOOSER = 427349;
+    public static final int OPEN_FILE_CHOOSER = 23323;
 }
--- web-support/src/main/java/com/proteus/web/view/ProteusWebView.java
+++ web-support/src/main/java/com/proteus/web/view/ProteusWebView.java
@@ -10,6 +10,7 @@ import android.webkit.WebView;
 import androidx.annotation.NonNull;
 
 import com.flipkart.android.proteus.ProteusView;
+import com.proteus.web.client.ProteusChromeClient;
 import com.proteus.web.client.ProteusWebViewClient;
 import com.proteus.web.util.WebViewDownloadListener;
 
@@ -37,6 +38,7 @@ public class ProteusWebView extends WebView implements ProteusView {
         settings.setJavaScriptEnabled(true);
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
         setWebViewClient(new ProteusWebViewClient());
+        setWebChromeClient(new ProteusChromeClient());
         setDownloadListener(new WebViewDownloadListener(context));
     }
 
