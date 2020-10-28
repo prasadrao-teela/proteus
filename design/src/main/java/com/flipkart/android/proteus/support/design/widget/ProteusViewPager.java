5c698de47ef27ea521fc6117b2492aceae29a57f
Sandy M <sandeepmaaram@gmail.com>
2020-10-28 22:35:37 +0530

    * clipToPadding and pageMargin added for ViewPager

--- design/src/main/java/com/flipkart/android/proteus/support/design/widget/ProteusViewPager.java
+++ design/src/main/java/com/flipkart/android/proteus/support/design/widget/ProteusViewPager.java
@@ -46,4 +46,27 @@ public class ProteusViewPager extends ViewPager implements ProteusView {
             setCurrentItem(getCurrentItem() - 1);
         }
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int mode = MeasureSpec.getMode(heightMeasureSpec);
+        // Unspecified means that the ViewPager is in a ScrollView WRAP_CONTENT.
+        // At Most means that the ViewPager is not in a ScrollView WRAP_CONTENT.
+        if (mode == MeasureSpec.UNSPECIFIED || mode == MeasureSpec.AT_MOST) {
+            // super has to be called in the beginning so the child views can be initialized.
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            int height = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int childMeasuredHeight = child.getMeasuredHeight();
+                if (childMeasuredHeight > height) {
+                    height = childMeasuredHeight;
+                }
+            }
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }
+        // super has to be called again so the new specs are treated as exact measurements
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 }
